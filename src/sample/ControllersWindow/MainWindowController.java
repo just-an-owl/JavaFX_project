a638bab76a322b764fc2826c9e56cec0b442cde4
just-an-owl <55064514+just-an-owl@users.noreply.github.com>
2020-12-19 23:52:58 +0300

    some change

--- src/sample/ControllersWindow/MainWindowController.java
+++ src/sample/ControllersWindow/MainWindowController.java
@@ -1,5 +1,7 @@
 package sample.ControllersWindow;
 
+import functions.FunctionPoint;
+import functions.InappropriateFunctionPointException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -8,6 +10,7 @@ import javafx.scene.control.*;
 
 import javafx.event.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Window;
 import javafx.stage.WindowEvent;
 import sample.TableViewFunctionPoint;
 import sample.Templates.WindowManager;
@@ -83,8 +86,62 @@ public class MainWindowController {
     }
 
     @FXML
-    private void clickAddButton(ActionEvent event){}
+    private void clickAddButton(ActionEvent event) throws InappropriateFunctionPointException {
+        if(observePoints!=null){
+            //observePoints
+            WindowManager.function.addPoint(new FunctionPoint(Double.parseDouble(xField.getText()), Double.parseDouble(yField.getText())));
+            tableViewFunctionPointspoints = new TableViewFunctionPoint[WindowManager.function.getPointCount()];
+            for (int i=0;i<WindowManager.function.getPointCount();++i){
+                tableViewFunctionPointspoints[i] = new TableViewFunctionPoint();
+                tableViewFunctionPointspoints[i].x.set(WindowManager.function.getPointX(i));
+                tableViewFunctionPointspoints[i].y.set(WindowManager.function.getPointY(i));
+            }
+            observePoints = FXCollections.observableArrayList(tableViewFunctionPointspoints);
+
+            points.setItems(observePoints);
+            xColumn.setCellValueFactory(new PropertyValueFactory("x"));
+            yColumn.setCellValueFactory(new PropertyValueFactory("y"));
+
+        }
+    }
 
     @FXML
-    private void clickDeleteButton(ActionEvent event){}
+    private void clickDeleteButton(ActionEvent event) throws IOException, InappropriateFunctionPointException {
+        double x = Double.parseDouble(xField.getText());
+        int i = 0;
+        boolean check = true;
+        while (i<WindowManager.function.getPointCount() && check) {
+            if (WindowManager.function.getPointX(i) > x) {
+                if ((int)x *100==(int)WindowManager.function.getPointX(i)*100) {
+                    if ((int) Double.parseDouble(yField.getText()) * 100 == (int) WindowManager.function.getPointY(i) * 100) {
+                        WindowManager.function.deletePoint(i);
+                        check = false;
+                    } else {
+                        WindowManager.showMassageBox("invalid y");
+
+                    }
+                }
+                else {
+                    WindowManager.showMassageBox("invalid x");
+                }
+            } else ++i;
+        }
+
+        if(observePoints!=null){
+            tableViewFunctionPointspoints = new TableViewFunctionPoint[WindowManager.function.getPointCount()];
+            for (int index=0;index<WindowManager.function.getPointCount();++index){
+                tableViewFunctionPointspoints[index] = new TableViewFunctionPoint();
+                tableViewFunctionPointspoints[index].x.set(WindowManager.function.getPointX(index));
+                tableViewFunctionPointspoints[index].y.set(WindowManager.function.getPointY(index));
+            }
+            observePoints = FXCollections.observableArrayList(tableViewFunctionPointspoints);
+
+            points.setItems(observePoints);
+            xColumn.setCellValueFactory(new PropertyValueFactory("x"));
+            yColumn.setCellValueFactory(new PropertyValueFactory("y"));
+
+        }
+    }
+
+
 }
